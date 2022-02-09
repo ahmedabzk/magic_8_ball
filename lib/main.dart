9b77d88c51a9191d750a019c6a8fa1419b3a3678
ahmedabzk <ahmed.abzk@gmail.com>
2022-02-09 12:49:02 +0300

    magic 8 ball

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: avoid_print
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          title: const Center(child: Text('Ask me anything')),
+        ),
+        body: const MagicBall(),
+      ),
+    ),
+  );
+}
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({Key? key}) : super(key: key);
+
+  @override
+  State<MagicBall> createState() => MagicBallState();
+}
+
+class MagicBallState extends State<MagicBall> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  ballNumber = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
